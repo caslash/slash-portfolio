e889491d28ede0157549bb7c2a87f758fd1e6c01
Cameron Slash <calexslash@aol.com>
2023-08-28 21:53:27 -0400

    Added validation to hello api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,14 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+
 type Data = {
-  name: string
+  message: string | undefined,
+  error: string | undefined
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  var name = req.query.name;
-  res.status(200).json({ name: `Hello, ${name}!` })
+  const name: string = String(req.query.name)
+
+  if (Object.keys(name).length === 0) {
+    res
+    .status(400)
+    .send({
+      error: 'Invalid name in request!',
+      message: undefined
+    })
+  } else {
+    res
+    .status(200)
+    .send({
+      message: `Hello, ${name}!`,
+      error: undefined
+    })
+  }
 }
