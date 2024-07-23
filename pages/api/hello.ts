693b779006e1af22b7b18c57ed56d0ddb14ed65d
Cameron Slash <caslash@sep.com>
2024-07-23 10:44:43 -0400

    Added new api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+type ResponseData = {
+  name: string,
+  age: number
 }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<ResponseData>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const name = req.query.name as string;
+  const age = req.query.age as string;
+  res.status(200).json({
+    name: name,
+    age: Number(age)
+  })
 }
