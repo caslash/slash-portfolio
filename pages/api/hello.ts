90c417fc5a9c6ee26ea0602880a33eb6dff2debd
Cameron Slash <caslash@sep.com>
2024-07-24 15:08:28 -0400

    Add api docs

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,14 +1,52 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type ResponseData = {
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    HelloResponse:
+ *      type: object
+ *      properties:
+ *        name:
+ *          type: string
+ *          description: Your name
+ *          example: Cameron
+ *        age:
+ *          type: integer
+ *          description: Your age
+ *          example: 24
+ */
+type HelloResponse = {
   name: string,
   age: number
 }
 
+/**
+ * @swagger
+ * /api/hello:
+ *  get:
+ *    description: Returns JSON object
+ *    parameters:
+ *      - in: query
+ *        name: name
+ *        description: Your name
+ *        type: string
+ *      - in: query
+ *        name: age
+ *        description: Your age
+ *        type: integer
+ *    responses:
+ *      200:
+ *        description: Returns HelloResponse object
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/HelloResponse'
+ */
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
+  res: NextApiResponse<HelloResponse>
 ) {
   const name = req.query.name as string;
   const age = req.query.age as string;
