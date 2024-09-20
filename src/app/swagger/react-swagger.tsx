5eff12664927503fbc9881cfb1d7e4bcd273ca5a
Cameron Slash <calexslash@aol.com>
2024-09-19 21:46:51 -0400

    Fix build, maybe

--- src/app/swagger/react-swagger.tsx
+++ src/app/swagger/react-swagger.tsx
@@ -1,14 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 type Props = {
-  spec: object;
+  spec: Record<string, any>;
+  url: string | undefined;
 };
 
-function ReactSwagger({ spec }: Props) {
-  return <SwaggerUI spec={spec} />;
+function ReactSwagger({ spec, url }: Props) {
+  if (process.env.NODE_ENV === "development") {
+    return <SwaggerUI spec={spec} />;
+  } else {
+    return <SwaggerUI url={url} />;
+  }
 }
 
 export default ReactSwagger;
