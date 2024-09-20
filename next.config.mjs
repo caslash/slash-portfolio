55b53cd11d9e1ca35714f35df6480b0568e440c8
Cameron Slash <calexslash@aol.com>
2024-09-19 22:06:58 -0400

    Fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   staticPageGenerationTimeout: 1000,
-  webpack: (config) => {
-    config.ignoreWarnings = [
-      { module: /node_modules\/swagger-jsdoc\/src\/utils\.js/ },
-      { file: /node_modules\/swagger-jsdoc\/src\/index\.js/ },
-    ];
-
-    return config;
-  },
+  transpilePackages: ["swagger-client", "swagger-ui-react"],
 };
 
 export default nextConfig;
