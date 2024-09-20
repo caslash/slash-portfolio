c616cfe4bc62d777c081358cc2d9c3f9850044ee
Cameron Slash <calexslash@aol.com>
2024-09-19 22:00:00 -0400

    Fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   staticPageGenerationTimeout: 1000,
   webpack: (config) => {
-    config.ignorWarnings = [
+    config.ignoreWarnings = [
       { module: /node_modules\/swagger-jsdoc\/src\/utils\.js/ },
       { file: /node_modules\/swagger-jsdoc\/src\/index\.js/ },
     ];
